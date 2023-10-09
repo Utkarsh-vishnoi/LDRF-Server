e54a7834cc4654c4528d7ad66f9493dcb3c0329d
Utkarsh Vishnoi <utkarshvishnoi25@gmail.com>
2023-10-09 08:21:52 +0530

    Code Updated

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
 const port = process.env.PORT || 3000;
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
-  socket.on('PING', (msg) => {
-    socket.broadcast.emit('PONG');
+  socket.on("PING", (msg) => {
+    socket.broadcast.emit("PONG");
   });
 });
 
 server.listen(port, () => {
-  console.log('listening on', port);
+  console.log("listening on", port);
 });
