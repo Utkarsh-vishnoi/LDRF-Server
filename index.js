a67338c7a5d5f08f8881e10a9232a922fff81744
Utkarsh Vishnoi <utkarshvishnoi25@gmail.com>
2023-10-09 08:01:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const port = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('PING', (msg) => {
+    socket.broadcast.emit('PONG');
+  });
+});
+
+server.listen(port, () => {
+  console.log('listening on', port);
+});
